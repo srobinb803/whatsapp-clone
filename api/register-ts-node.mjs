3af512a60d695d976d3e26f7cbc95f089419e2e3
Robin <srobin803@gmail.com>
2025-08-13 16:34:09 +0530

    update vercel.json

--- api/register-ts-node.mjs
+++
@@ -1,5 +0,0 @@
-import { register } from "node:module";
-import { pathToFileURL } from "node:url";
-
-// register the ts-node ESM loader from this project root
-register("ts-node/esm", pathToFileURL("./"));
